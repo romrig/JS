d18b474e3d30415a28899f5422481a2be7783722
romrig <romansitorigal@gmail.com>
2022-06-23 14:53:10 -0300

    Simulador interactivo

--- Complementario 1/main.js
+++ Complementario 1/main.js
@@ -1,11 +1,17 @@
-// Contador de turnos , una vez llegado al turno numero 10 se corta el cupo.
-for (let i = 1; i <= 10; i++) {
-   
-    let ingresarNombre = prompt("Ingresar nombre y apellido");
-    
-    alert(" Turno  N° "+i+" Nombre del solicitante: "+ingresarNombre);
-    if(i === 10){
-        alert("Ya no hay mas turnos , por favor vuelva a nuevamente mañana y capaz tengamos nuevos turnos disponibles");
-        break;
+function pedirDatos() {
+    let nombre = prompt("Hola como estas? Ingrese su nombre");
+    alert("Hola " + nombre + " , espero que estes bien , bienvenido a la pagina web de Raymundo Full Shop , presione Enter o Aceptar para continuar");
+    let articulos = prompt(nombre +" que articulo estas buscando? ingrese el numero de el producto para poder derivarlo a un asesor directo! 1-Libreria , 2-Jugueteria , 3-Corte laser en mdf")
+    if(articulos == 1){
+        alert("Para articulos de Libreria , comunicarse al 3415039494 , o ingresar en nuestra pagina de instagram , @Diserhome")
+    } else if (articulos == 2){
+        alert("Para productos de jugueteria , seguinos en instagram y enterate de todas las novedades , @RustikoFullShop")
+    } else if ( articulos == 3) {
+        alert("Para productos en corte laser de madera fibro facil , podes comunicarte al 3415693995 o seguirnos en instagram , @Diserhome")
+    } else {
+        alert("Por favor , ingrese un numero valido.")
     }
+    
 }
+
+pedirDatos()
